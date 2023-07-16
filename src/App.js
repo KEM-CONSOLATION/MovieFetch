71ce60c8a5af0c1f01fb1461b0791ce60e3610b8
Kem-consolation <consolationlotachi@gmail.com>
2023-07-16 15:20:31 +0100

    Added the movie title to the page title

--- src/App.js
+++ src/App.js
@@ -342,6 +342,10 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
     getMovieDetails();
   }, [selectedId]);
 
+  useEffect(() => {
+    if (!title) return;
+    document.title = `Movie ${title}`;
+  }, [title]);
   return (
     <div className=" details">
       {isLoading || movie === null ? (
